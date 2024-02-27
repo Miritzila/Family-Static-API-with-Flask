e9ca2a36b3ce20c165441f6fdd984fbc3db70d0b
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-02-27 12:35:14 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,27 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+jackson_family.add_member({
+    "id": jackson_family._generateId(),
+    "first_name": "John Jackson",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+})
+
+jackson_family.add_member({
+    "id": 0,
+    "first_name": "Jane Jackson",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+})
+
+jackson_family.add_member({
+    "id": 0,
+    "first_name": "Jimmy Jackson",
+    "age": 5,
+    "lucky_numbers": [1]
+})
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -27,16 +48,29 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
+@app.route('/member/<int:member_id>', methods=['GET','DELETE'])
+def handle_member(member_id):
+
+    if request.method == "GET":
+        member = jackson_family.get_member(member_id)
+        response_body = member
+        return jsonify(response_body), 200
+    
+    if request.method == "DELETE":
+        jackson_family.delete_member(member_id)
+        return jsonify({'done':True}), 200
+
+@app.route('/member/', methods=['POST'])
+def handle_add_member():
+
+    member = request.json
+    print(member)
+    jackson_family.add_member(member)
+    return jsonify("Ha sido agregado con éxito"), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -7,30 +7,32 @@ update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 from random import randint
+from types import MemberDescriptorType
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        # example list of members
         self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        self._members.append(member)
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members: 
+            if member.get("id") == id:
+                self._members.remove(member)    
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if id == member["id"]:
+                return member
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+    
